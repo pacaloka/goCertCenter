ca2f965cad65362260d4680c9aa6fb9e7ea08f5f
Andreas Mallek <andreas.mallek@certcenter.com>
2018-02-22 10:09:10 +0000

    Improvements; Not relevant

--- examples/Reissue.go
+++ examples/Reissue.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	certcenter "certcenter.com/go"
 	"fmt"
 	_ "io/ioutil"
 	_ "time"
+	certcenter "certcenter.com/go"
 )
 
 // Set your valid OAuth2 Bearer
@@ -16,13 +16,21 @@ func init() {
 
 func main() {
 	// Reissue a particular order
-	//
+  //
 	res, _ := certcenter.Reissue(&certcenter.ReissueRequest{
 		CertCenterOrderID: 123456789,
-		OrderParameters: certcenter.ReissueOrderParameters{
-			CSR:                    "#CSR#",
-			DVAuthMethod:           "EMAIL",
+		OrderParameters:certcenter.ReissueOrderParameters{
+			CSR: "#CSR#",
+			DVAuthMethod: "EMAIL",
 			SignatureHashAlgorithm: "SHA256-FULL-CHAIN",
+			DomainApprovers: &certcenter.DomainApprovers{
+				DomainApprover: []certcenter.DomainApproverItem{certcenter.DomainApproverItem{
+					Domain: "certcenter.com",
+					Approvers: []certcenter.Approver{certcenter.Approver{
+						ApproverEmail: "domains@certcenter.com",
+					}},
+				}},
+			},
 		},
 		ReissueEmail: "valid-approver@example.com",
 	})
